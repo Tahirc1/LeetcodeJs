70a2c3a524693d8d14cb5ce22126fecea8ad09f0
Tahir <83111631+Tahirc1@users.noreply.github.com>
2023-10-11 18:28:47 +0530

    Adding comments to 1. Two Sum.js

--- 1-10/1. Two Sum.js
+++ 1-10/1. Two Sum.js
@@ -1,9 +1,12 @@
 var twoSum = function(nums, target) {
-    let map = new Map()
+    let map = new Map() // Create a map
     for(let i = 0 ; i <  nums.length ; i++){
+        // if target-nums[i] is in map
         if(map.get(target-nums[i]) >= 0 ){
+            // return answer
             return [map.get(target-nums[i]),i]
         }
+        // set nums[i] = i in map
         map.set(nums[i],i);
     }
 }
