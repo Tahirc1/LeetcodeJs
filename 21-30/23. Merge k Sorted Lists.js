e066e5eae49cf87acba81df4fe82266f3ee38e1f
Tahir <83111631+Tahirc1@users.noreply.github.com>
2023-10-20 11:21:50 +0530

    update code & comment 23. Merge k Sorted Lists.js

--- 21-30/23. Merge k Sorted Lists.js
+++ 21-30/23. Merge k Sorted Lists.js
@@ -1,19 +1,25 @@
 var mergeKLists = function(lists) {
-    let a = []
+    let a = [] // create an array
+    // loop over all the lists
     for(let i = 0 ; i < lists.length ; i++){
-        let  k = lists[i]
+        let  k = lists[i] // store list in k
+        // loop till end of list in k
         while(k != null){
+            // push listnode value in a
             a.push(k.val)
+            // go to next node
             k = k.next
         }
     }
+    // sort the array
     a.sort(function(a,b){return a-b})
-    let m = new ListNode(0)
-    let  ans = m
+    let m = new ListNode(0) // create listnode
+    let ans = m // store head of listnode in ans
+    // loop the array
     for(let j = 0 ; j< a.length ;j++){
-        let n = new ListNode(a[j])
-        m.next = n
-        m = m.next
+    // assign array j value to next listnode and go to next node
+        m = m.next = new ListNode(a[j])
     }
+    // return ans List
     return ans.next
 };
